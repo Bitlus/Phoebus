19dd6d62f97ab751cb9a55f18994d1f5e383533f
James Olsen <jdwo@live.com>
2024-03-24 16:04:23 +1300

    Added test for custom character set where it's using a pre-defined one + some extra characters

--- Phoebus.Tests/StringExtensionsTests.cs
+++ Phoebus.Tests/StringExtensionsTests.cs
@@ -1,72 +1,79 @@
 ﻿using Phoebus;
 
-namespace Phoebus.Tests
+namespace Phoebus.Tests;
+
+public class StringExtensionsTests
 {
-    public class StringExtensionsTests
+    [Fact]
+    public void StringIsAlpha()
     {
-        [Fact]
-        public void StringIsAlpha()
-        {
-            var test = "test";
-            Assert.True(test.IsAlphabetical());
-        }
+        var test = "test";
+        Assert.True(test.IsAlphabetical());
+    }
 
-        [Fact]
-        public void StringIsNotAlpha()
-        {
-            var test = "test123%%";
-            Assert.False(test.IsAlphabetical());
-        }
+    [Fact]
+    public void StringIsNotAlpha()
+    {
+        var test = "test123%%";
+        Assert.False(test.IsAlphabetical());
+    }
 
-        [Fact]
-        public void StringHasSpaces()
-        {
-            var test = "I have spaces";
-            Assert.False(test.IsAlphabetical());
-        }
+    [Fact]
+    public void StringHasSpaces()
+    {
+        var test = "I have spaces";
+        Assert.False(test.IsAlphabetical());
+    }
 
-        [Fact]
-        public void StringIsAlphaNumeric()
-        {
-            var test = "test123";
-            Assert.True(test.IsAlphanumeric());
-        }
+    [Fact]
+    public void StringIsAlphaNumeric()
+    {
+        var test = "test123";
+        Assert.True(test.IsAlphanumeric());
+    }
 
-        [Fact]
-        public void StringIsNotAlphaNumeric()
-        {
-            var test = "test123%%";
-            Assert.False(test.IsAlphanumeric());
-        }
+    [Fact]
+    public void StringIsNotAlphaNumeric()
+    {
+        var test = "test123%%";
+        Assert.False(test.IsAlphanumeric());
+    }
 
-        [Fact]
-        public void StringIsNumeric()
-        {
-            var test = "123";
-            Assert.True(test.IsNumeric());
-        }
+    [Fact]
+    public void StringIsNumeric()
+    {
+        var test = "123";
+        Assert.True(test.IsNumeric());
+    }
 
-        [Fact]
-        public void StringIsNotNumeric()
-        {
-            var test = "123%%";
-            Assert.False(test.IsNumeric());
-        }
+    [Fact]
+    public void StringIsNotNumeric()
+    {
+        var test = "123%%";
+        Assert.False(test.IsNumeric());
+    }
 
-        [Fact]
-        public void StringIsInCharacterSet()
-        {
-            var test = "test";
-            var characterSet = "tes";
-            Assert.True(test.IsInCharacterSet(characterSet));
-        }
+    [Fact]
+    public void StringIsInCharacterSet()
+    {
+        var test = "test";
+        var characterSet = "tes";
+        Assert.True(test.IsInCharacterSet(characterSet));
+    }
 
-        [Fact]
-        public void StringIsNotInCharacterSet()
-        {
-            var test = "test";
-            var characterSet = "abc";
-            Assert.False(test.IsInCharacterSet(characterSet));
-        }
+    [Fact]
+    public void StringIsNotInCharacterSet()
+    {
+        var test = "test";
+        var characterSet = "abc";
+        Assert.False(test.IsInCharacterSet(characterSet));
+    }
+
+    [Fact]
+    public void CustomCharSetTest()
+    {
+        var test = "test._-123";
+        var characterSet = PhoebusCharSets.Alphanumeric + "._-";
+        Assert.True(test.IsInCharacterSet(characterSet));
     }
 }
